51ba514d2a6ee9792d6416902b68d94a4003539f
slawkens1 <slawkens@gmail.com>
2017-12-29 11:07:43 +0100

    * update to 0.7.5-dev
    * fixed faq containing html code
    * update item_images_url config to 1092
    * added ttf, woff and ico to the list of ignored files
    * fixed infinite loop in init.php

--- config.php
+++ config.php
@@ -75,7 +75,7 @@ $config = array(
 
 	// images
 	'outfit_images_url' => 'http://outfit-images.ots.me/outfit.php', // set to animoutfit.php for animated outfit
-	'item_images_url' => 'http://item-images.ots.me/960/', // set to images/items if you host your own items in images folder
+	'item_images_url' => 'http://item-images.ots.me/1092/', // set to images/items if you host your own items in images folder
 
 	// account
 	'account_management' => true, // disable if you're using other method to manage users (fe. tfs account manager)
--- system/init.php
+++ system/init.php
@@ -14,8 +14,7 @@ if(file_exists(BASE . 'config.local.php')) // user customizations
 	require(BASE . 'config.local.php');
 
 if(!isset($config['installed']) || !$config['installed']) {
-	header('Location: ' . BASE_URL);
-	die('AAC has not been installed yet or there was error during installation. Please install again.');
+	die('MyAAC has not been installed yet or there was error during installation. Please install again.');
 }
 
 date_default_timezone_set($config['date_timezone']);
