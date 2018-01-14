4bfc804d856467e1fb358ccf46f7c457692e98b8
slawkens1 <slawkens@gmail.com>
2018-01-14 09:45:16 +0100

    * auto add z_polls table on install

--- install/tools/7-finish.php
+++ install/tools/7-finish.php
@@ -85,6 +85,9 @@ if(!databaseMigration20($content)) {
 				<textarea cols="70" rows="10">' . $content . '</textarea>');
 }
 
+// add z_polls tables
+require_once(SYSTEM . 'migrations/22.php');
+
 $locale['step_finish_desc'] = str_replace('$ADMIN_PANEL$', generateLink(str_replace('tools/', '',ADMIN_URL), $locale['step_finish_admin_panel'], true), $locale['step_finish_desc']);
 $locale['step_finish_desc'] = str_replace('$HOMEPAGE$', generateLink(str_replace('tools/', '', BASE_URL), $locale['step_finish_homepage'], true), $locale['step_finish_desc']);
 $locale['step_finish_desc'] = str_replace('$LINK$', generateLink('http://my-aac.org', 'http://my-aac.org', true), $locale['step_finish_desc']);
