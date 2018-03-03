815b1f9b5bb512beb3ffd66df5195fd26b4727ac
slawkens1 <slawkens@gmail.com>
2018-03-03 11:31:01 +0100

    * update to 0.7.10

--- common.php
+++ common.php
@@ -26,7 +26,7 @@
 session_start();
 
 define('MYAAC', true);
-define('MYAAC_VERSION', '0.7.10-dev');
+define('MYAAC_VERSION', '0.7.10');
 define('DATABASE_VERSION', 22);
 define('TABLE_PREFIX', 'myaac_');
 define('START_TIME', microtime(true));
