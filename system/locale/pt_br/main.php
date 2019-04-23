0205fc93c12554bc0bfd8b8f6f25333a46d30796
Ivens Pontes <ivenscardoso@hotmail.com>
2019-04-23 09:40:42 -0300

    add locale pt_br

---
+++ system/locale/pt_br/main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * english language file
+ * main.php
+ *
+ * @author Slawkens <slawkens@gmail.com>
+ */
+$locale['name']     = 'Brazilian Portuguese';
+$locale['lang']		= 'pt-br';
+$locale['encoding'] = 'utf-8';
+$locale['direction']= 'ltr';
+
+$locale['error404'] = 'A página solicitada não foi encontrada.';
+$locale['news'] = 'Últimas notícias';
+?>
