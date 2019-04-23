9620ac7d25550baa2ea5f755bf451f11ee39d32e
slawkens <slawkens@gmail.com>
2019-04-23 23:52:13 +0200

    * fix include path not found in install_plugin.php when running from other directory

--- system/bin/install_plugin.php
+++ system/bin/install_plugin.php
@@ -1,22 +1,22 @@
 <?php
 
-if(php_sapi_name() != "cli") {
+if(PHP_SAPI !== 'cli') {
 	die('This script can be run only in command line mode.');
 }
 
-require_once '../../common.php';
+require_once __DIR__ . '/../../common.php';
 require_once SYSTEM . 'functions.php';
 require_once SYSTEM . 'init.php';
 require_once SYSTEM . 'hooks.php';
 require_once LIBS . 'plugins.php';
 
-if($argc != 2) {
+if($argc !== 2) {
 	exit('This command expects one parameter: zip file name (plugin)' . PHP_EOL);
 }
 
 $path_to_file = $argv[1];
 $ext = strtolower(pathinfo($path_to_file, PATHINFO_EXTENSION));
-if($ext != 'zip') {// check if it is zipped/compressed file
+if($ext !== 'zip') {// check if it is zipped/compressed file
 	exit('Please install only .zip files.' . PHP_EOL);
 }
 
@@ -36,4 +36,3 @@ else
 	echo 'ERROR: ' . Plugins::getError();
 
 echo PHP_EOL;
-?>
