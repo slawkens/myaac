b61c6151eb23ac84335914c8ea606916a6446d9b
slawkens <slawkens@gmail.com>
2020-02-12 23:45:43 +0100

    This was wrong, Mr. @whiteblxk
    As its not accounts table but players :)

--- system/pages/admin/modules/lastlogin.php
+++ system/pages/admin/modules/lastlogin.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($db->hasColumn('players', 'lastlogin')) {
-	$players = $db->query('SELECT ' . (USE_ACCOUNT_NAME ? 'name' : 'id') . ' as name, level,lastlogin FROM players ORDER BY lastlogin DESC LIMIT 10;');
+	$players = $db->query('SELECT name, level, lastlogin FROM players ORDER BY lastlogin DESC LIMIT 10;');
 } else {
 	$players = 0;
 }
