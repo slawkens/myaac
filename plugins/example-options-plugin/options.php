7469f132540c1cf94ee5d0a29977d4c58018acc2
slawkens <slawkens@gmail.com>
2020-05-15 00:06:03 +0200

    Forgot the plugin example of options

---
+++ plugins/example-options-plugin/options.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+	'name' => 'gesior_shop',
+	'options' => [
+		'donation_type' => [
+			'name' => 'Donation Type',
+			'type' => 'options',
+			'options' => ['points' => 'Points', 'coins' => 'Coins'],
+			'default' => 'points',
+			'desc' => 'What should be added to player account?',
+		],
+		'default_outfit_colors' => [
+			'name' => 'Default Outfit Colors',
+			'type' => 'text',
+			'default' => '4,38,87,114',
+			'desc' => "Default colors of outfits/addons in addons category<br/>
+			doesn't matter for othire and other servers without addons<br/>
+			you can use this outfit generator: http://sleqqus.idl.pl/tlg<br/>
+			Format: head,body,legs,feet",
+		],
+		'just_testing_boolean' => [
+			'name' => 'Just Testing Boolean',
+			'type' => 'boolean',
+			'default' => false,
+			'desc' => "Some description.",
+		],
+		'just_testing_number' => [
+			'name' => 'Just Testing Number',
+			'type' => 'number',
+			'default' => 999,
+			'desc' => "Some description.",
+		],
+	],
+];
