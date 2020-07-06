722264a0835d202df2f6782e6f8a3e7855aa3a70
whiteblXK <krzys16001@gmail.com>
2020-07-07 00:31:50 +0200

    Added limit to search characters (#134)
    * Update characters.php
    
    * Update config.php
    
    * Variable name change, better use LIMIT in query instead in loop
    
    * Just to be sure. Security first :)
    
    * use config function
    
    Co-authored-by: slawkens <slawkens@gmail.com>
    (cherry picked from commit dc536f0fc01862f052d5edee29ac188ac63fcf66)

--- config.php
+++ config.php
@@ -150,6 +150,9 @@ $config = array(
 		3 => 'Paladin Sample',
 		4 => 'Knight Sample'
 	),
+	
+	// it must show limited number of players after using search in character page
+	'characters_search_limit' => 15,
 
 	// town list used when creating character
 	// won't be displayed if there is only one item (rookgaard for example)
