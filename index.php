5d5875d5409cb768cb2c2704c5a68b88c9ec4b75
slawkens <slawkens@gmail.com>
2020-10-30 06:52:50 +0100

    Bans page working for TFS 1.x + move to Twig (#140)
    * Bans page working for TFS 1.x + move to Twig
    
    * Remove some debug code
    
    * Add some protection
    
    * Better check.

--- index.php
+++ index.php
@@ -97,6 +97,7 @@ else {
 			'/^account\/character\/comment\/[A-Za-z0-9-_%+\']+\/?$/' => array('subtopic' => 'accountmanagement', 'action' => 'change_comment', 'name' => '$3'),
 			'/^account\/character\/comment\/?$/' => array('subtopic' => 'accountmanagement', 'action' => 'change_comment'),
 			'/^account\/confirm_email\/[A-Za-z0-9-_]+\/?$/' => array('subtopic' => 'accountmanagement', 'action' => 'confirm_email', 'v' => '$2'),
+			'/^bans\/[0-9]+\/?$/' => array('subtopic' => 'bans', 'page' => '$1'),
 			'/^characters\/[A-Za-z0-9-_%+\']+$/' => array('subtopic' => 'characters', 'name' => '$1'),
 			'/^changelog\/[0-9]+\/?$/' => array('subtopic' => 'changelog', 'page' => '$1'),
 			'/^commands\/add\/?$/' => array('subtopic' => 'commands', 'action' => 'add'),
