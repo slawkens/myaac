aa17ddbf2471c3ca450a278f347d6e55b891cf86
slawkens <slawkens@gmail.com>
2021-02-23 15:10:03 +0100

    Fix compatibility with PHP 7.0 and lower

--- system/libs/pot/OTS_DB_PDOQuery.php
+++ system/libs/pot/OTS_DB_PDOQuery.php
@@ -1,16 +1,7 @@
 <?php
 
 if (PHP_VERSION_ID >= 80000) {
-	trait OTS_DB_PDOQuery
-	{
-		/**
-		 * @return PDOStatement
-		 */
-		public function query(?string $query = null, ?int $fetchMode = null, mixed ...$fetchModeArgs)
-		{
-			return $this->doQuery($query, $fetchMode, ...$fetchModeArgs);
-		}
-	}
+	require LIBS . 'pot/OTS_DB_PDOQuery_PHP71.php';
 } else {
 	trait OTS_DB_PDOQuery
 	{
