118e8c487e39cd3816d0ffdcacf9b311d07c7caa
slawkens <slawkens@gmail.com>
2022-11-28 12:41:29 +0100

    Fixes for racaptcha

--- config.php
+++ config.php
@@ -141,7 +141,7 @@ $config = array(
 	'recaptcha_site_key' => '', // get your own site and secret keys at https://www.google.com/recaptcha
 	'recaptcha_secret_key' => '',
 	// following option apply only for ReCaptcha v2-checkbox
-	'recaptcha_theme' => 'light', // light, dark
+	'recaptcha_v2_theme' => 'light', // light, dark
 	// following option apply only for ReCaptcha v3
 	// min score for validation, between 0 - 1.0
 	// https://developers.google.com/recaptcha/docs/v3#interpreting_the_score
