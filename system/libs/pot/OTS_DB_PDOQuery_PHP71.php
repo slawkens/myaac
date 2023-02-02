4028a58adc8c3e40eae889fa23f4e2b35db6e8cc
slawkens <slawkens@gmail.com>
2023-02-02 16:20:09 +0100

    Update OTS_DB_PDOQuery_PHP71.php

--- system/libs/pot/OTS_DB_PDOQuery_PHP71.php
+++ system/libs/pot/OTS_DB_PDOQuery_PHP71.php
@@ -5,7 +5,7 @@ trait OTS_DB_PDOQuery
 	/**
 	 * @return PDOStatement
 	 */
-	public function query(?string $query = null, ?int $fetchMode = null, mixed ...$fetchModeArgs)
+	public function query(?string $query = null, ?int $fetchMode = null, mixed ...$fetchModeArgs): PDOStatement
 	{
 		return $this->doQuery($query, $fetchMode, ...$fetchModeArgs);
 	}
