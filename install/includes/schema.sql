7401add73d7a8f1ce21535056cf2bc15e5ddaf6a
Matheus Collier <matheuscollier@gmail.com>
2023-03-29 18:42:48 -0300

    [UPDATE] Adding monster looks to db

--- install/includes/schema.sql
+++ install/includes/schema.sql
@@ -1,4 +1,4 @@
-SET @myaac_database_version = 34;
+SET @myaac_database_version = 35;
 
 CREATE TABLE `myaac_account_actions`
 (
---
+++ system/migrations/35.php
@@ -0,0 +1,3 @@
+<?php
+// add look column
+$db->exec('ALTER TABLE `' . TABLE_PREFIX . "monsters` ADD `look` VARCHAR(255) NOT NULL AFTER `loot`;");
