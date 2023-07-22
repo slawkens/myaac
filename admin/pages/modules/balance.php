4a629b4418a60dfcb16a2d6f3fec121b7c689ec9
slawkens <slawkens@gmail.com>
2023-07-22 14:03:57 +0200

    Add protection, nothing important - thrown exception before

--- admin/pages/modules/balance.php
+++ admin/pages/modules/balance.php
@@ -1,4 +1,6 @@
 <?php
+defined('MYAAC') or die('Direct access not allowed!');
+
 $balance = ($db->hasColumn('players', 'balance') ? $db->query('SELECT `balance`, `id`, `name`,`level` FROM `players` ORDER BY `balance` DESC LIMIT 10;') : 0);
 
 $twig->display('balance.html.twig', array(
