e9f155fb49aa99838a710a0568923e87bc0718f3
slawkens <slawkens@gmail.com>
2023-09-16 10:20:34 +0200

    Fix XSS in players editor

--- admin/pages/players.php
+++ admin/pages/players.php
@@ -872,7 +872,7 @@ else if (isset($_REQUEST['search'])) {
 					<form action="<?php echo $player_base; ?>" method="post">
 						<label for="name">Player Name:</label>
 						<div class="input-group input-group-sm">
-							<input type="text" class="form-control" name="search" value="<?php echo $search_player; ?>" maxlength="32" size="32">
+							<input type="text" class="form-control" id="search" name="search" value="<?= escapeHtml($search_player); ?>" maxlength="32" size="32">
 							<span class="input-group-append"><button type="submit" class="btn btn-info btn-flat">Search</button></span>
 						</div>
 					</form>
