3fdf1d3f44d9114f769c017288e6ce91d8ab47ee
slawkens <slawkens@gmail.com>
2023-11-05 20:13:31 +0100

    require_once is better

--- admin/pages/items.php
+++ admin/pages/items.php
@@ -10,8 +10,8 @@
 defined('MYAAC') or die('Direct access not allowed!');
 $title = 'Load items.xml';
 
-require LIBS . 'items.php';
-require LIBS . 'weapons.php';
+require_once LIBS . 'items.php';
+require_once LIBS . 'weapons.php';
 
 $twig->display('admin.items.html.twig');
 
