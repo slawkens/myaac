a88103a95622bcf20db64a0b25d9592f2dca21bf
Danilo Pucci <danilopucci@users.noreply.github.com>
2024-01-01 19:31:26 -0300

    - adding check before flush buffer (#245)

--- install/tools/7-finish.php
+++ install/tools/7-finish.php
@@ -8,8 +8,10 @@ require BASE . 'install/includes/functions.php';
 require BASE . 'install/includes/locale.php';
 
 ini_set('max_execution_time', 300);
-ob_implicit_flush();
-ob_end_flush();
+if(ob_get_length() > 0) {
+	ob_implicit_flush();
+	ob_end_flush();
+}
 header('X-Accel-Buffering: no');
 
 if(isset($config['installed']) && $config['installed'] && !isset($_SESSION['saved'])) {
