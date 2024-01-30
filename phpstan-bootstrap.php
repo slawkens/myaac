c08f099d98eea0a216623190714ab598e6a732e1
slawkens <slawkens@gmail.com>
2024-01-31 00:37:29 +0100

    phpstan v1 + workflow

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/system/libs/pot/OTS.php';
+$ots = POT::getInstance();
+
+require __DIR__ . '/system/libs/pot/InvitesDriver.php';
+require __DIR__ . '/system/libs/rfc6238.php';
+
+require __DIR__ . '/common.php';
+
+const ACTION = '';
+define('SELF_NAME', basename(__FILE__));
