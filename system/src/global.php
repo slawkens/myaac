44fe03c6ea5eb14d95a2025947b4cf83e900d3d9
slawkens <slawkens@gmail.com>
2024-06-29 15:53:02 +0200

    feat: Hooks filters

--- system/src/global.php
+++ system/src/global.php
@@ -82,6 +82,16 @@ define('HOOK_CACHE_CLEAR', ++$i);
 define('HOOK_INSTALL_FINISH', ++$i);
 define('HOOK_INSTALL_FINISH_END', ++$i);
 
+// hook filters
+define('HOOK_FILTER_TWIG_DISPLAY', ++$i);
+define('HOOK_FILTER_TWIG_RENDER', ++$i);
+define('HOOK_FILTER_THEME_FOOTER', ++$i);
+
+const HOOK_FILTERS = [
+	HOOK_FILTER_TWIG_DISPLAY,
+	HOOK_FILTER_TWIG_RENDER,
+];
+
 const HOOK_FIRST = HOOK_STARTUP;
 define('HOOK_LAST', $i);
 
