ee22b363f1026518630233d9c02f6ae205bba863
Jonatas <103670242+Scrollog@users.noreply.github.com>
2024-08-15 23:00:49 -0300

    Highscore frags fixed

--- system/pages/highscores.php
+++ system/pages/highscores.php
@@ -180,7 +180,7 @@ if (empty($highscores)) {
 	} else if ($skill == SKILL_FRAGS) // frags
 	{
 		if ($db->hasTable('player_killers')) {
-			$query->addSelect(['value' => PlayerKillers::where('player_killers.player_id', 'players.id')->selectRaw('COUNT(*)')]);
+			$query->addSelect(['value' => PlayerKillers::whereColumn('player_killers.player_id', 'players.id')->selectRaw('COUNT(*)')]);
 		} else {
 			$query->addSelect(['value' => PlayerDeath::unjustified()->whereColumn('player_deaths.killed_by', 'players.name')->selectRaw('COUNT(*)')]);
 		}
