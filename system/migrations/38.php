d6270a7e32f3141433abae957742e1ec1b5095c3
slawkens <slawkens@gmail.com>
2024-11-19 15:33:12 +0100

    Fix duplicate function declaration

--- system/migrations/38.php
+++ system/migrations/38.php
@@ -6,7 +6,7 @@
 // 2023-11-11
 // execute highscores_ids_hidden once again, cause of settings
 $up = function () {
-	require __DIR__ . '/20.php';
+	require_once __DIR__ . '/20.php';
 	updateHighscoresIdsHidden();
 };
 
