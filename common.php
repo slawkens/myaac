2e4bbeb7f7f9b8b668b818edce349c0e7c7749f8
slawkens <slawkens@gmail.com>
2024-11-19 14:10:25 +0100

    Release v0.8.19

--- common.php
+++ common.php
@@ -26,7 +26,7 @@
 if (version_compare(phpversion(), '7.2.5', '<')) die('PHP version 7.2.5 or higher is required.');
 
 define('MYAAC', true);
-define('MYAAC_VERSION', '0.8.19-dev');
+define('MYAAC_VERSION', '0.8.19');
 define('DATABASE_VERSION', 33);
 define('TABLE_PREFIX', 'myaac_');
 define('START_TIME', microtime(true));
