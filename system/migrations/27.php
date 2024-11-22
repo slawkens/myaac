c0a66037e0e4d4691ea0ee3be10dfdb99c2084a1
slawkens <slawkens@gmail.com>
2024-11-22 16:04:48 +0100

    Fix column not found - hidden/hide

--- system/migrations/27.php
+++ system/migrations/27.php
@@ -16,7 +16,7 @@ $up = function () use ($db) {
 			'player_id' => 1,
 			'php' => 0,
 			'access' => 0,
-			'hidden' => 0,
+			($db->hasColumn(TABLE_PREFIX . 'pages', 'hide') ? 'hide' : 'hidden') => 0,
 		]);
 	}
 
@@ -30,7 +30,7 @@ $up = function () use ($db) {
 			'player_id' => 1,
 			'php' => 0,
 			'access' => 0,
-			'hidden' => 0,
+			($db->hasColumn(TABLE_PREFIX . 'pages', 'hide') ? 'hide' : 'hidden') => 0,
 		]);
 	}
 };
