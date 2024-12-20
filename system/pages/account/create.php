783d96fc6568a607d3198b832fed3a0dd06c4ebb
slawkens <slawkens@gmail.com>
2024-12-20 22:42:35 +0100

    Set default_socket_timeout for ipinfo.io checkup

--- system/pages/account/create.php
+++ system/pages/account/create.php
@@ -331,6 +331,8 @@ if(setting('core.account_country_recognize')) {
 		$country_recognized = $country_session;
 	}
 	else {
+		ini_set('default_socket_timeout', 5);
+
 		$info = json_decode(@file_get_contents('http://ipinfo.io/' . get_browser_real_ip() . '/geo'), true);
 		if(isset($info['country'])) {
 			$country_recognized = strtolower($info['country']);
