da1816cc1319862b1a706f05e545ae7d4a8c75ad
slawkens <slawkens@gmail.com>
2024-12-20 22:42:46 +0100

    use https

--- system/pages/account/create.php
+++ system/pages/account/create.php
@@ -333,7 +333,7 @@ if(setting('core.account_country_recognize')) {
 	else {
 		ini_set('default_socket_timeout', 5);
 
-		$info = json_decode(@file_get_contents('http://ipinfo.io/' . get_browser_real_ip() . '/geo'), true);
+		$info = json_decode(@file_get_contents('https://ipinfo.io/' . get_browser_real_ip() . '/geo'), true);
 		if(isset($info['country'])) {
 			$country_recognized = strtolower($info['country']);
 			setSession('country', $country_recognized);
