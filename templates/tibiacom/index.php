dd47423157e007a36cea60fd248191a278fef891
slawkens <slawkens@gmail.com>
2025-01-17 11:16:19 +0100

    Fix menus like char-bazaar/auctions-history etc.

--- templates/tibiacom/index.php
+++ templates/tibiacom/index.php
@@ -42,8 +42,6 @@ if(isset($config['boxes']))
 						} else if ($exp[0] === 'news' && $exp[1] === 'archive') {
 							$tmp = 'news_archive';
 						}
-						else
-							$tmp = $exp[0];
 					}
 				}
 			}
