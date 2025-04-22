68170e42cd8fac43c55654ba595f09425618f26e
slawkens <slawkens@gmail.com>
2025-04-22 13:45:35 +0200

    Fix headline.php: change image format to .png cause of black background

--- templates/tibiacom/headline.php
+++ templates/tibiacom/headline.php
@@ -31,12 +31,12 @@ if(!@file_exists($page_file))
 	imagettftext($image, 18, 0, 4, 20, imagecolorallocate($image, 240, 209, 164), $font, $_GET['t']);
 
 	// header mime type
-	header('Content-type: image/gif');
+	header('Content-type: image/png');
 
 	// save image
-	imagegif($image/*, $file*/);
+	imagepng($image/*, $file*/);
 //}
 
 // output image
-//header('Content-type: image/gif');
+//header('Content-type: image/png');
 //readfile($file);
