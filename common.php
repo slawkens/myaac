7c92d1c19734b912b1ed0587e6ba680fc18d409f
slawkens <slawkens@gmail.com>
2025-05-14 15:11:20 +0200

    Start v1.5.1-dev

--- common.php
+++ common.php
@@ -26,7 +26,7 @@
 if (version_compare(phpversion(), '8.1', '<')) die('PHP version 8.1 or higher is required.');
 
 const MYAAC = true;
-const MYAAC_VERSION = '1.5';
+const MYAAC_VERSION = '1.5.1-dev';
 const DATABASE_VERSION = 45;
 const TABLE_PREFIX = 'myaac_';
 define('START_TIME', microtime(true));
