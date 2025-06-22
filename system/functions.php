566c2a9151ab6392286f74e26853faa19a1b4f24
slawkens <slawkens@gmail.com>
2025-06-22 08:48:24 +0200

    Move out of $cache->enabled

--- system/functions.php
+++ system/functions.php
@@ -1263,10 +1263,10 @@ function clearCache()
 
 		global $db;
 		$db->setClearCacheAfter(true);
+	}
 
-		if (function_exists('apcu_clear_cache')) {
-			apcu_clear_cache();
-		}
+	if (function_exists('apcu_clear_cache')) {
+		apcu_clear_cache();
 	}
 
 	deleteDirectory(CACHE . 'signatures', ['index.html'], true);
