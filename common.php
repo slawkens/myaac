bb830bce4425bec02c42e99e68e9e18fd3eea7dc
slawkens <slawkens@gmail.com>
2025-06-22 08:55:29 +0200

    Release v1.7

--- common.php
+++ common.php
@@ -26,7 +26,7 @@
 if (version_compare(phpversion(), '8.1', '<')) die('PHP version 8.1 or higher is required.');
 
 const MYAAC = true;
-const MYAAC_VERSION = '1.6.1';
+const MYAAC_VERSION = '1.7';
 const DATABASE_VERSION = 45;
 const TABLE_PREFIX = 'myaac_';
 define('START_TIME', microtime(true));
