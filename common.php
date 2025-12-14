9ed06782e67772826d927ad847a077b99df5060d
slawkens <slawkens@gmail.com>
2025-12-14 10:21:23 +0100

    Ini set html_errors = 0, to show html code in exceptions

--- common.php
+++ common.php
@@ -154,6 +154,7 @@ if(isset($config['env']) && $config['env'] !== 'dev' && !defined('MYAAC_INSTALL'
 	error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 }
 else {
+	ini_set('html_errors', 0);
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
