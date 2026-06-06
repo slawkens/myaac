a1c1c8fcf744f963858aaa19ed532063c622af1f
slawkens <slawkens@gmail.com>
2026-06-06 08:14:32 +0200

    Fix: Open Redirect via redirect Parameter
    Also thanks to @Gesior for reporting this

--- system/pages/account/manage.php
+++ system/pages/account/manage.php
@@ -23,7 +23,7 @@ if(isset($_REQUEST['redirect']))
 	$redirect = urldecode($_REQUEST['redirect']);
 
 	// should never happen, unless hacker modify the URL
-	if (!str_contains($redirect, BASE_URL)) {
+	if (!str_starts_with($redirect, BASE_URL)) {
 		error('Fatal error: Cannot redirect outside the website.');
 		return;
 	}
