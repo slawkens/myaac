6f2bfd21eb7657e98a71ab09c20e2b39fd1cfbdd
slawkens <slawkens@gmail.com>
2026-06-06 08:25:56 +0200

    Do not save sessions in myaac system folder
    PHP default will be used

--- common.php
+++ common.php
@@ -84,7 +84,6 @@ const TOOLS = BASE . 'tools/';
 const VENDOR = BASE . 'vendor/';
 
 // other dirs
-const SESSIONS_DIR = SYSTEM . 'php_sessions';
 const GUILD_IMAGES_DIR = 'images/guilds/';
 const EDITOR_IMAGES_DIR = 'images/editor/';
 const GALLERY_DIR = 'images/gallery/';
@@ -118,7 +117,6 @@ const SMTP_SECURITY_TLS = 2;
 const ACCOUNT_NUMBER_LENGTH = 8;
 
 if (!IS_CLI) {
-	session_save_path(SESSIONS_DIR);
 	session_start();
 }
 
