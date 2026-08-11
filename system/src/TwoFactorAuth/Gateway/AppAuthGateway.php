e018b88b8caa5acc0a9519ac4f466a2846888c97
slawkens <slawkens@gmail.com>
2026-08-11 20:03:28 +0200

    Use 2fa_secret column from accounts table

--- system/src/TwoFactorAuth/Gateway/AppAuthGateway.php
+++ system/src/TwoFactorAuth/Gateway/AppAuthGateway.php
@@ -9,7 +9,7 @@ class AppAuthGateway extends BaseAuthGateway implements AuthGatewayInterface
 {
 	public function verifyCode(string $code): bool
 	{
-		$otp = TOTP::createFromSecret($this->account->getCustomField('secret'));
+		$otp = TOTP::createFromSecret($this->account->getCustomField('2fa_secret'));
 
 		$otp->setLabel($this->account->getEmail());
 		$otp->setIssuer(configLua('serverName'));
