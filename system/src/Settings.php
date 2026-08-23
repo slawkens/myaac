dbd4013fa9f9f81c3f99f7a6e38a3f5d5ba54df6
slawkens <slawkens@gmail.com>
2026-08-23 18:54:06 +0200

    Settings: Support for more HTML input types & attributes
    * Refactor settings code to support all the possible html input type values: color, range, month etc.
    * select/bool/integer/int is accepted as setting type
    * For now only the "file" type is missing

--- system/src/Settings.php
+++ system/src/Settings.php
@@ -210,21 +210,22 @@ class Settings implements \ArrayAccess
 						<td>
 							<?php
 				}
+
 				if (isset($setting['hidden']) && $setting['hidden']) {
 					$value = '';
 					if ($setting['type'] === 'boolean') {
 						$value = (getBoolean($setting['default']) ? 'true' : 'false');
 					}
-					else if (in_array($setting['type'], ['text', 'number', 'float', 'double', 'email', 'password', 'textarea'])) {
-						$value = $setting['default'];
-					}
 					else if ($setting['type'] === 'options') {
 						$value = $setting['options'][$setting['default']];
+					}
+					else {
+						$value = $setting['default'];
 					}
 
 					echo '<input type="hidden" name="settings[' . $key . ']" value="' . $value . '" id="' . $key . '"';
 				}
-				else if ($setting['type'] === 'boolean') {
+				elseif ($setting['type'] === 'boolean' || $setting['type'] === 'bool') {
 					if(isset($settingsDb[$key])) {
 						$value = getBoolean($settingsDb[$key]);
 					}
@@ -236,34 +237,7 @@ class Settings implements \ArrayAccess
 					$checkbox($key, false, $value, $setting['enabled'] ?? true);
 				}
 
-				else if (in_array($setting['type'], ['text', 'number', 'float', 'double', 'email', 'password', 'date', 'datetime-local', 'time'])) {
-					if (in_array($setting['type'], ['float', 'double'])) {
-						$setting['type'] = 'number';
-					}
-
-					if ($setting['type'] === 'number') {
-						$min = (isset($setting['min']) ? ' min="' . $setting['min'] . '"' : '');
-						$max = (isset($setting['max']) ? ' max="' . $setting['max'] . '"' : '');
-						$step = (isset($setting['step']) ? ' step="' . $setting['step'] . '"' : '');
-					}
-					else {
-						$min = $max = $step = '';
-					}
-
-					if ($setting['type'] === 'password') {
-						echo '<div class="input-group" id="show-hide-' . $key . '">';
-					}
-
-					$enabled = $setting['enabled'] ?? true;
-
-					echo '<input class="form-control" type="' . $setting['type'] . '" name="settings[' . $key . ']" value="' . escapeHtml($settingsDb[$key] ?? ($setting['default'] ?? '')) . '" id="' . $key . '"' . $min . $max . $step . ' ' . ($enabled ? '' : 'disabled') . '/>';
-
-					if ($setting['type'] === 'password') {
-						echo '<div class="input-group-append input-group-text"><a href=""><i class="fas fa-eye-slash" ></i></a></div></div>';
-					}
-				}
-
-				else if($setting['type'] === 'textarea') {
+				elseif ($setting['type'] === 'textarea') {
 					if (isset($settingsDb[$key]) && is_array($settingsDb[$key])) {
 						$settingsDb[$key] = implode(',', $settingsDb[$key]);
 					}
@@ -280,7 +254,7 @@ class Settings implements \ArrayAccess
 					echo '<textarea class="form-control" rows="' . $rows . '" name="settings[' . $key . ']" id="' . $key . '" ' . ($enabled ? '' : 'disabled') . '>' . escapeHtml($value) . '</textarea>';
 				}
 
-				else if ($setting['type'] === 'options') {
+				elseif ($setting['type'] === 'options' || $setting['type'] === 'select') {
 					if ($setting['options'] === '$templates') {
 						$templates = [];
 						foreach (get_templates() as $value) {
@@ -303,7 +277,7 @@ class Settings implements \ArrayAccess
 
 						$setting['options'] = $clients;
 					}
-					else if ($setting['options'] == '$timezones') {
+					else if ($setting['options'] === '$timezones') {
 						$timezones = [];
 						foreach (\DateTimeZone::listIdentifiers() as $value) {
 							$timezones[$value] = $value;
@@ -341,6 +315,63 @@ class Settings implements \ArrayAccess
 					echo '</select>';
 				}
 
+				elseif ($setting['type'] === 'custom') {
+					if (!isset($setting['custom']) || !is_callable($setting['custom'])) {
+						throw new \RuntimeException("Setting $plugin.$key: attribute 'custom' must be callable");
+					}
+
+					if (!isset($settingsDb[$key])) {
+						$settingsDb[$key] = ($setting['default'] ?? '');
+					}
+
+					echo $setting['custom']($key, $settingsDb[$key]);
+				}
+
+				else {
+					if (in_array($setting['type'], ['float', 'double', 'integer', 'int'])) {
+						$setting['type'] = 'number';
+					}
+
+					$extraAttributes = '';
+
+					foreach ([
+						'min',
+						'max',
+						'step',
+						'size',
+						'minlength',
+						'maxlength',
+						'placeholder',
+						'pattern',
+						'autocomplete'
+					] as $attr) {
+						$extraAttributes .= (isset($setting[$attr]) ? ' ' . $attr . '="' . $setting[$attr] . '"' : '');
+					}
+
+					foreach ([
+						'readonly',
+						'disabled',
+						'required',
+						'autofocus'
+					] as $attr) {
+						if (isset($setting[$attr]) && !is_bool($setting[$attr])) {
+							throw new \RuntimeException("Setting $plugin.$key: attribute '$attr' must be boolean");
+						}
+
+						$extraAttributes .= (isset($setting[$attr]) && $setting[$attr] ? ' ' . $attr : '');
+					}
+
+					if ($setting['type'] === 'password') {
+						echo '<div class="input-group" id="show-hide-' . $key . '">';
+					}
+
+					echo '<input class="form-control" type="' . $setting['type'] . '" name="settings[' . $key . ']" value="' . escapeHtml($settingsDb[$key] ?? ($setting['default'] ?? '')) . '" id="' . $key . '"' . $extraAttributes . '/>';
+
+					if ($setting['type'] === 'password') {
+						echo '<div class="input-group-append input-group-text"><a href=""><i class="fas fa-eye-slash" ></i></a></div></div>';
+					}
+				}
+
 				if (!isset($setting['hidden']) || !$setting['hidden']) {
 				?>
 						</td>
@@ -353,13 +384,13 @@ class Settings implements \ArrayAccess
 								if ($setting['type'] === 'boolean') {
 									echo ($setting['default'] ? 'Yes' : 'No');
 								}
-								else if (in_array($setting['type'], ['text', 'number', 'float', 'double', 'email', 'password', 'textarea'])) {
-									echo $setting['default'];
-								}
-								else if ($setting['type'] === 'options') {
+								elseif ($setting['type'] === 'options') {
 									if (is_int($setting['default']) || !empty($setting['default'])) {
 										echo $setting['options'][$setting['default']];
 									}
+								}
+								else {
+									echo $setting['default'] ?? '';
 								}
 								?></div>
 						</td>
@@ -495,16 +526,19 @@ class Settings implements \ArrayAccess
 		if(isset($ret['type'])) {
 			switch($ret['type']) {
 				case 'boolean':
+				case 'bool':
 					$ret['value'] = getBoolean($ret['value']);
 					break;
 
 				case 'number':
+				case 'integer':
+				case 'int':
 					$ret['value'] = (int)$ret['value'];
 					break;
 
 				case 'double':
 				case 'float':
-					$ret['value'] = (double)($ret['value']);
+					$ret['value'] = (float)($ret['value']);
 					break;
 
 				default:
