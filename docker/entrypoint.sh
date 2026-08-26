b8a1d06d98f2c932c4f4f78aa099a14eb07b8108
slawkens <slawkens@gmail.com>
2026-08-26 19:52:00 +0200

    Do not clone server + refactor code sync-files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p /home/www-data/server/data/XML
+
+cat > /home/www-data/server/data/XML/vocations.xml <<'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<vocations>
+	<vocation id="0" name="None" fromvoc="0" />
+	<vocation id="1" name="Sorcerer" fromvoc="1" />
+	<vocation id="2" name="Druid" fromvoc="2" />
+	<vocation id="3" name="Paladin" fromvoc="3" />
+	<vocation id="4" name="Knight" fromvoc="4" />
+	<vocation id="5" name="Master Sorcerer" fromvoc="1" />
+	<vocation id="6" name="Elder Druid" fromvoc="2" />
+	<vocation id="7" name="Royal Paladin" fromvoc="3" />
+	<vocation id="8" name="Elite Knight" fromvoc="4" />
+</vocations>
+EOF
+
+cat > /home/www-data/server/data/XML/groups.xml <<'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<groups>
+	<group id="1" name="player" access="0" maxdepotitems="0" maxvipentries="0" />
+	<group id="6" name="god" access="1" maxdepotitems="0" maxvipentries="200" />
+</groups>
+EOF
+
+mkdir -p /home/www-data/server/data/items
+curl --output /home/www-data/server/data/items/items.xml https://raw.githubusercontent.com/otland/forgottenserver/refs/heads/master/data/items/items.xml
+
+mkdir -p /myaac-main-folder
+cat > /usr/local/bin/sync-windows-files.sh <<'EOF'
+#!/bin/bash
+while true; do
+  rsync -au --exclude-from="/myaac-main-folder/.dockerignore" "/myaac-main-folder/" "/var/www/html"
+  sleep 2;
+done
+EOF
+
+exec "$@"
