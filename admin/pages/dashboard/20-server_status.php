e86edc26c899628546c9fc6db05b72b8c8ef91d7
slawkens <slawkens@gmail.com>
2026-09-16 12:24:35 +0200

    Convert dashboard server_status to twig

--- admin/pages/dashboard/20-server_status.php
+++ admin/pages/dashboard/20-server_status.php
@@ -1,58 +1,7 @@
 <?php
 defined('MYAAC') or die('Direct access not allowed!');
-if (isset($status)) {
+if (!isset($status)) {
+	$status = [];
+}
 
-	$error_icon = '<i class="fas fa-exclamation-circle text-danger"></i>'; ?>
-	<div data-id="server_status" class="sortable-handle col-md-6 col-lg-6">
-		<div class="card card-info card-outline">
-			<div class="card-header border-bottom-0">
-				<div class="row">
-					<div class="col">
-						<h5 class="font-weight-bold m-0">Server Status</h5>
-					</div>
-					<div class="col-6">
-						<span class="float-right small"><b>Last checked</b>: <?php echo(isset($status['lastCheck']) ? date("l, d.m.Y H:i:s", $status['lastCheck']) : $error_icon); ?></span>
-					</div>
-					<div class="col">
-						<button type="button" class="close close-card" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div class="card-body p-0 ">
-				<table class="table">
-					<tbody>
-					<tr>
-						<th width="30%">Server</th>
-						<td><?php echo(isset($status['server']) & isset($status['serverVersion']) ? $status['server'] . ' x ' . $status['serverVersion'] : $error_icon) ?></td>
-
-					</tr>
-					<tr>
-						<th>Client</th>
-						<td><?php echo(isset($status['clientVersion']) ? $status['clientVersion'] : $error_icon) ?></td>
-					</tr>
-					<tr>
-						<th>Map</th>
-						<td>
-							<?php if (isset($status['mapName']) & isset($status['mapAuthor']) & isset($status['mapWidth']) & isset($status['mapHeight'])) {
-								echo $status['mapName'] . ' by <b>' . $status['mapAuthor'] . '</b><br/>' . $status['mapWidth'] . ' x ' . $status['mapHeight'];
-							} else {
-								echo $error_icon;
-							} ?>
-						</td>
-					</tr>
-					<tr>
-						<th>Monsters</th>
-						<td><?php echo (isset($status['monsters']) ? $status['monsters'] : $error_icon); ?></td>
-					</tr>
-					<tr>
-						<th>MOTD:</th>
-						<td><?php echo(isset($status['motd']) ? $status['motd'] : $error_icon); ?></td>
-					</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-<?php } ?>
+$twig->display('server_status.html.twig', ['status' => $status]);
