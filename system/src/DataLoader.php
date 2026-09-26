9113ef55cf4cf6d648f5204f31194f0927d25fe6
slawkens <slawkens@gmail.com>
2026-09-26 10:15:08 +0200

    Update DataLoader.php

--- system/src/DataLoader.php
+++ system/src/DataLoader.php
@@ -74,7 +74,7 @@ class DataLoader
 
 	private static function getLoadedTime()
 	{
-		$endTime = round(microtime(true) - self::$startTime, 3);
+		$endTime = round(microtime(true) - self::$startTime, 4);
 		return ' (' . str_replace('$TIME$', $endTime, self::$locale['loaded_in_seconds']) . ')';
 	}
 }
